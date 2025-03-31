38f63a92c1c9f5b91bf7b305a09d05e196a3673c
ndmccarthy <nicole.deere21@gmail.com>
2025-03-31 11:20:55 -0500

    update test file

--- Data_Mining/testing.py
+++ Data_Mining/testing.py
@@ -1,11 +1,16 @@
-from data_understanding import *
+from data_understanding import calculate
+from data_preprocessing import *
 
 csv = "test_data.csv"
 column = 2
 
-# Testing the func() function
-x, y, title, x_label, y_label = func()
-plt.scatter(x, y)
-plt.title(title)
-plt.xlabel(x_label)
-plt.ylabel(y_label)
+numObj, minValue, maxValue, mean, stdev, Q1, median, Q3, IQR = calculate(csv, 2)
+print(f"Mean: {mean}")
+print(f"\nStDev: {stdev}")
+print(f"\nMin: {minValue}")
+print(f"\nMax: {maxValue}")
+
+mm = normalization(csv, "Current_Age", 'min_max')
+print(mm)
+z = normalization(csv, "Current_Age", 'z-score')
+print(z)
